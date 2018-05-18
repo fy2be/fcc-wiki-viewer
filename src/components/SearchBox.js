a531665dbf3dfff0d7f2836acfc27836e5d3cb81
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-18 19:01:29 +0200

    add styles

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,13 +3,11 @@ import React from 'react';
 class SearchBox extends React.Component {
     render() {
         return (
-            <div className='search-box'>
-                <form className='search-form'>
-                    <input type='text' className='search' placeholder='What are you looking for?' />
-                    <ul className='search-suggestions'>
-                    </ul>
-                </form>
-            </div>
+            <form className='search-form'>
+                <input type='text' className='search' placeholder='What are you looking for?' />
+                <ul className='search-suggestions'>
+                </ul>
+            </form>
         );
     }
 }
