efd54db01b57e4aa078fa049bf0dbf64c64bcb2d
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-18 23:36:25 +0200

    add alt to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => (
     <div className='logo'>
-        <img src='/wiki.svg' />
+        <img src='/wiki.svg' alt='Wikipedia - The Free Encyclopedia' />
     </div>
 );
 
