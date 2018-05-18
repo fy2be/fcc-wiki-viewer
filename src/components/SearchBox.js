a0d944398714cf7e0367a1cbbfb10b4aa47d4e0a
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-18 23:23:57 +0200

    search suggestions is not needed

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 class SearchBox extends React.Component {
+
     render() {
         return (
             <form className='search-form'>
                 <input type='text' className='search' placeholder='What are you looking for?' />
-                <ul className='search-suggestions'>
-                </ul>
             </form>
         );
     }
