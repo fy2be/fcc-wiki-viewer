463c0c6f7dd12db9ec71ec7bdd94b10c2b269ea4
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-18 23:35:17 +0200

    fix security risk

--- src/components/RandomSearch.js
+++ src/components/RandomSearch.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const RandomSearch = () => (
     <div className='random-search'>
-        <a href='https://en.wikipedia.org/wiki/Special:Random' target='_blank'>
+        <a
+            href='https://en.wikipedia.org/wiki/Special:Random'
+            target='_blank'
+            rel='noopener noreferrer'
+        >
             Random Article
         </a>
     </div>
