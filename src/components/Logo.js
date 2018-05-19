3d76ae81fc5e523e0986bb0bd2d924f61d34a9dd
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-19 15:17:52 +0200

    change Logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => (
     <div className='logo'>
-        <img src='/wiki.svg' alt='Wikipedia - The Free Encyclopedia' />
+        <img src='/wiki.svg' alt='Wikipedia - The Free Encyclopedia' className='big' />
     </div>
 );
 
